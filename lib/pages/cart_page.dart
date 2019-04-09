a1bf7606479f570533f9781a59a77208e3902dd5
hokori-poi <sntdawuk@qq.com>
2019-04-09 22:49:36 +0800

    50

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,45 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:provide/provide.dart';
-import '../provide/counter.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Column(
-        children: <Widget>[Number(), MyButton()],
-      )),
-    );
-  }
+  _CartPageState createState() => _CartPageState();
 }
 
-class Number extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 200),
-      child: Provide<Counter>(builder: (context, child, counter) {
-        return Text(
-          '${counter.value}',
-          style: Theme.of(context).textTheme.display1,
-        );
-      }),
-    );
-  }
-}
+class _CartPageState extends State<CartPage> {
 
-class MyButton extends StatelessWidget {
+  List<String> testList = [];
+
   @override
   Widget build(BuildContext context) {
+    _show();
     return Container(
-      child: RaisedButton(
-        onPressed: () {
-          Provide.value<Counter>(context).increment();
-        },
-        child: Text('PLUS'),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 500,
+            child: ListView.builder(
+              itemCount: testList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(testList[index]),
+                );
+              },
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              _add();
+            },
+            child: Text('增加'),
+          ),
+          RaisedButton(
+            onPressed: () {
+              _clear();
+            },
+            child: Text('清空'),
+          )
+        ],
       ),
     );
   }
+
+  // 增加方法
+  void _add() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String temp = '233';
+    testList.add(temp);
+    prefs.setStringList('list', testList);
+    _show();
+  }
+
+  // 查询
+  void _show() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.getStringList('list') != null) {
+      setState(() {
+        testList = prefs.getStringList('list');
+      });
+    } 
+  }
+
+  // 删除
+  void _clear() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //prefs.clear();
+    prefs.remove('list');
+    setState(() {
+      testList = [];
+    });
+  }
+
 }
--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -5,9 +5,9 @@ import './details_component/details_top_area.dart';
 import './details_component/details_description.dart';
 import './details_component/details_tabbar.dart';
 import './details_component/details_web.dart';
+import './details_component/details_bottom.dart';
 
 class DetailsPage extends StatelessWidget {
-
   final String goodsId;
 
   DetailsPage(this.goodsId);
@@ -28,15 +28,24 @@ class DetailsPage extends StatelessWidget {
         future: _getDetailInfo(context),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Container(
-              child: ListView(
-                children: <Widget>[
-                  DetailsTopArea(),
-                  DetailsDescription(),
-                  DetailsTabbar(),
-                  DetailsWeb()
-                ],
-              ),
+            return Stack(
+              children: <Widget>[
+                Container(
+                  child: ListView(
+                    children: <Widget>[
+                      DetailsTopArea(),
+                      DetailsDescription(),
+                      DetailsTabbar(),
+                      DetailsWeb()
+                    ],
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  bottom: 0,
+                  child: DetailsBottom(),
+                )
+              ],
             );
           } else {
             return Text('加载中。。。');
@@ -50,5 +59,4 @@ class DetailsPage extends StatelessWidget {
     await Provide.value<DetailsProvide>(context).getGoodsDetail(goodsId);
     return '完成加载';
   }
-
 }
