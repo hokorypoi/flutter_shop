aac2e103d59b201da467c586bd35f11fa2687a11
hokori <sntdawuk@qq.com>
2019-04-14 23:47:58 +0800

    53

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,76 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provide/provide.dart';
+import '../provide/cart_provide.dart';
 
-class CartPage extends StatefulWidget {
-  @override
-  _CartPageState createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
-
-  List<String> testList = [];
-
+class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    _show();
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 500,
-            child: ListView.builder(
-              itemCount: testList.length,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('购物车')
+      ),
+      body: FutureBuilder(
+        future: _getCartInfo(context),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List cartList = Provide.value<CartProvide>(context).cartList;
+            return ListView.builder(
+              itemCount: cartList.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text(testList[index]),
+                  title: Text(cartList[index].goodsName),
                 );
               },
-            ),
-          ),
-          RaisedButton(
-            onPressed: () {
-              _add();
-            },
-            child: Text('增加'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              _clear();
-            },
-            child: Text('清空'),
-          )
-        ],
+            );
+          } else {
+            return Text('正在加载');
+          }
+        },
       ),
     );
   }
 
-  // 增加方法
-  void _add() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String temp = '233';
-    testList.add(temp);
-    prefs.setStringList('list', testList);
-    _show();
-  }
-
-  // 查询
-  void _show() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    if (prefs.getStringList('list') != null) {
-      setState(() {
-        testList = prefs.getStringList('list');
-      });
-    } 
-  }
-
-  // 删除
-  void _clear() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    //prefs.clear();
-    prefs.remove('list');
-    setState(() {
-      testList = [];
-    });
+  Future<String> _getCartInfo(BuildContext context) async {
+    await Provide.value<CartProvide>(context).getCartInfo();
+    return 'end';
   }
 
 }
+
--- lib/provide/cart_provide.dart
+++ lib/provide/cart_provide.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import '../model/cart_info.dart';
 
 class CartProvide with ChangeNotifier {
   String cartString = '[]';
+  List<CartInfoModel> cartList = [];
 
   save(goodsId, goodsName, count, price, images) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -15,23 +17,28 @@ class CartProvide with ChangeNotifier {
     tempList.forEach((item) {
       if (item['goodsId'] == goodsId) {
         tempList[i]['count'] = item['count'] + 1;
+        cartList[i].count++;
         exists = true;
       }
       i++;
     });
 
+    Map<String, dynamic> newGoods = {
+      'goodsId': goodsId,
+      'goodsName': goodsName,
+      'count': count,
+      'price': price,
+      'images': images
+    };
+
     if (!exists) {
-      tempList.add({
-        'goodsId': goodsId,
-        'goodsName': goodsName,
-        'count': count,
-        'price': price,
-        'images': images
-      });
+      tempList.add(newGoods);
+      cartList.add(CartInfoModel.fromJson(newGoods));
     }
 
     cartString = json.encode(tempList).toString();
-    print(cartString);
+    print('字符串：${cartString}');
+    print('数据模型：${cartList}');
     prefs.setString('cartInfo', cartString);
     notifyListeners();
   }
@@ -39,7 +46,23 @@ class CartProvide with ChangeNotifier {
   remove() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove('cartInfo');
+    cartList = [];
     print('清空完成------------------');
+    notifyListeners();
+  }
+
+  getCartInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    cartString = prefs.getString('cartInfo');
+    cartList = [];
+    if (cartString == null) {
+      
+    } else {
+      List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
+      tempList.forEach((item) {
+        cartList.add(CartInfoModel.fromJson(item));
+      });
+    }
     notifyListeners();
   }
 }
